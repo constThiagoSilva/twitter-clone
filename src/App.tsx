f6a94d2af9a5f299cae18f2873b7fdd2bc766e88
drakeira <thiago.p.dasilva2005@gmail.com>
2022-10-10 17:23:49 -0300

    feat: set padding screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export const App = () => {
   return (
-    <div className="App">
-      Hello, Twitter
+    <div className="w-full h-screen bg-gray-900 px-2 md:px-10 lg:px-24 xl:px-80">
+      <p className="text-white">Hello</p>
     </div>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'white': '#F5F8FA',
+      'gray-900': '#14171A',
+      'gray-600': '#657786',
+      'gray-400': '#AAB8C2',
+      'gray-200': '#E1E8ED',
+      'blue-500': '#1DA1F2'
+    }
   },
   plugins: [],
 }
