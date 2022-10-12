bc9f5d4d59d2092227ae0afc86a9168c01a2db55
drakeira <thiago.p.dasilva2005@gmail.com>
2022-10-12 19:04:54 -0300

    fix: wh-sidebar colors

--- src/components/WhatHappeningSideBar/index.tsx
+++ src/components/WhatHappeningSideBar/index.tsx
@@ -6,26 +6,26 @@ import ProfileImage from "../../assets/profile.jpeg";
 export const WhatHappeningSideBar = () => {
   return (
     <div className="hidden lg:flex h-full w-[30%] items-center flex-col p-2">
-      <div className="w-full h-[50px] rounded-full bg-gray-600 flex items-center  p-2">
+      <div className="w-full h-[50px] rounded-full bg-gray-600 flex items-center  p-4">
         <div>
-          <SearchIcon className="text-[18px] font-light" />
+          <SearchIcon className="text-[18px] text-gray-300" />
         </div>
         <input
           type="text"
           placeholder="Buscar no Twitter"
-          className="w-[90%] bg-gray-600 placeholder:font-light focus:outline-none text-[18px] ml-2 text-white"
+          className="w-[90%] bg-gray-600 placeholder:text-gray-300 focus:outline-none text-[18px] ml-4 text-white"
         />
       </div>
 
-      <section className="w-full mt-2 bg-gray-600 rounded-md flex flex-col ">
+      <section className="w-full mt-2 bg-gray-700 rounded-xl flex flex-col ">
         <h2 className="font-bold text-white p-2">O que está acontecendo</h2>
 
-        <div className="mt-4 hover:bg-gray-400 transition-all p-2">
+        <div className="mt-4 hover:bg-gray-600 transition-all p-2">
           <div className="w-full cursor-pointer flex items-center ">
             <div className="w-full flex-1 flex flex-col">
-              <div className="flex items-center">
-                <span className="font-thin text-[16px]">Eleições 20...</span>
-                <span className="font-thin text-[16px]">Noite anterior</span>
+              <div className="flex items-center  text-gray-300">
+                <span className=" text-[16px]">Eleições 20...</span>
+                <span className=" text-[16px]">Noite anterior</span>
               </div>
               <p className="text-sm text-white font-bold">Futebol</p>
             </div>
@@ -37,37 +37,37 @@ export const WhatHappeningSideBar = () => {
               />
             </div>
           </div>
-          <div className="w-full text-[16px] mt-2 font-thin">
+          <div className="w-full text-[16px] mt-2 text-gray-300">
             <span>
               Assuntos do momento:{" "}
-              <strong className="text-blue-500 font-thin">Tarciso</strong>
+              <strong className="text-blue-500 font-light">Tarciso</strong>
             </span>
           </div>
         </div>
 
-        <div className="mt-4 hover:bg-gray-400 transition-all p-2 overflow-hidden">
+        <div className="mt-4 hover:bg-gray-600 transition-all p-2 overflow-hidden">
           <div className="w-full cursor-pointer flex items-center ">
             <div className="w-full flex-1 flex flex-col">
-              <div className="flex items-center">
-                <span className="font-thin text-[16px]">Eleições 20...</span>
-                <span className="font-thin text-[16px]">Noite anterior</span>
+              <div className="flex items-center text-gray-300">
+                <span className=" text-[16px]">Eleições 20...</span>
+                <span className=" text-[16px]">Noite anterior</span>
               </div>
               <p className="text-sm text-white font-bold">Futebol</p>
             </div>
             <div>
-              <DotsIcon className="text-gray-600 text-[24px] cursor-pointer" />
+              <DotsIcon className="text-gray-300 text-[24px] cursor-pointer" />
             </div>
           </div>
-          <div className="w-full text-[16px] mt-2 font-thin">
+          <div className="w-full text-[16px] mt-2 text-gray-300">
             <span>20 mil tweets</span>
           </div>
         </div>
       </section>
 
-      <section className="w-full mt-2 bg-gray-600 rounded-md flex flex-col ">
+      <section className="w-full mt-2 bg-gray-700 rounded-lg flex flex-col ">
         <h2 className="font-bold text-white p-2">Quem seguir</h2>
 
-        <div className="mt-4 hover:bg-gray-400 transition-all p-2">
+        <div className="mt-4 hover:bg-gray-600 transition-all p-2">
           <div className="w-full cursor-pointer flex items-center ">
             <div className="h-[60px] w-[60px] rounded-full overflow-hidden">
               <img
@@ -78,7 +78,7 @@ export const WhatHappeningSideBar = () => {
             </div>
             <div className="w-full flex-1 flex flex-col ml-2 mr-2">
               <h3 className="text-sm text-white font-bold">thigas</h3>
-              <span className="font-thin text-[16px]">@o_thigaz</span>
+              <span className=" text-[16px] text-gray-300">@o_thigaz</span>
             </div>
             <button className="w-[90px] h-[40px] bg-white rounded-2xl text-[18px] font-medium">
               seguir
@@ -94,7 +94,7 @@ export const WhatHappeningSideBar = () => {
                 className="w-full h-full"
               />
             </div>
-            <button className="ml-4 flex-1 h-[40px] bg-gray-600 border border-gray-400 text-white rounded-2xl text-[18px] font-medium flex items-center justify-center overflow-hidden">
+            <button className="ml-4 flex-1 h-[40px] bg-gray-700 border border-gray-300 text-white rounded-2xl text-[18px] font-medium flex items-center justify-center overflow-hidden">
                 <span className="w-full h-full flex items-center justify-center">
               seguindo
                 </span>
